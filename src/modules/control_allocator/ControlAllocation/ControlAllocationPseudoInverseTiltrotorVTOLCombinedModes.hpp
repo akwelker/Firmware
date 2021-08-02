d47f40f18328eb1d662eb2a3aa2b994dc471e531
Mason Peterson <mbpeterson70@gmail.com>
2021-08-02 17:54:03 -0600

    Fixed ctrlalloc combined modes elevon computation
    Correctly computes elevon values based off of mix solution in tiltrotor
    pseudoinverse control allocation class. No longer uses elevon max angle
    and right elevon is negated.
    
    Fixed allocated thrust and torque calculations in same file.
    
    Cleaned up code.
    
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- src/modules/control_allocator/ControlAllocation/ControlAllocationPseudoInverseTiltrotorVTOLCombinedModes.hpp
+++ src/modules/control_allocator/ControlAllocation/ControlAllocationPseudoInverseTiltrotorVTOLCombinedModes.hpp
@@ -56,72 +56,49 @@ public:
 	virtual void allocate() override {
         float deg2rad = (float) M_PI / 180.f;
         float tilt_servo_max = _param_ca_tlt_srvo_max.get() * deg2rad;
-        // float elevon_max = _param_ca_elevon_max.get() * deg2rad;
 
         //Compute new gains if needed
         updatePseudoInverse();
 
-        // for(uint8_t i = 0; i < 16; ++i)
-        //     PX4_INFO("mix[%d]: %.5f %.5f %.5f %.5f %.5f %.5f", i,
-        //     (double) _mix(i, 0), (double) _mix(i, 1), (double) _mix(i, 2), (double) _mix(i, 3), (double) _mix(i, 4), (double) _mix(i, 5));
-        // for(uint8_t i = 0; i < 6; ++i)
-        //     PX4_INFO("effectiveness_matrix[%d]: %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f", i,
-        //     (double) _effectiveness(i, 0),
-        //     (double) _effectiveness(i, 1),
-        //     (double) _effectiveness(i, 2),
-        //     (double) _effectiveness(i, 3),
-        //     (double) _effectiveness(i, 4),
-        //     (double) _effectiveness(i, 5),
-        //     (double) _effectiveness(i, 6),
-        //     (double) _effectiveness(i, 7),
-        //     (double) _effectiveness(i, 8),
-        //     (double) _effectiveness(i, 9),
-        //     (double) _effectiveness(i, 10),
-        //     (double) _effectiveness(i, 11),
-        //     (double) _effectiveness(i, 12),
-        //     (double) _effectiveness(i, 13),
-        //     (double) _effectiveness(i, 14),
-        //     (double) _effectiveness(i, 15)
-        //     );
-
         // Allocate
         matrix::Vector<float, NUM_ACTUATORS> mix_solution =
             _actuator_trim + _mix * (_control_sp - _control_trim);
-        // PX4_INFO("mix_solution: %.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f",
-        //     (double) mix_solution(0),
-        //     (double) mix_solution(1),
-        //     (double) mix_solution(2),
-        //     (double) mix_solution(3),
-        //     (double) mix_solution(4),
-        //     (double) mix_solution(5),
-        //     (double) mix_solution(6),
-        //     (double) mix_solution(7));
-        // PX4_INFO("trig_stuff: %.5f %.5f", (double) (atan2f(mix_solution(1), mix_solution(0))), (double) (atan2f(mix_solution(3), mix_solution(2))));
 
-        _actuator_sp(0) = sqrt(mix_solution(0) * mix_solution(0) + mix_solution(1) * mix_solution(1)); //1.0f;
-        _actuator_sp(1) = sqrt(mix_solution(2) * mix_solution(2) + mix_solution(3) * mix_solution(3)); //1.0f;
-        _actuator_sp(2) = mix_solution(4); //1.0f;
-        _actuator_sp(3) = 0.f; //0.f;
-        _actuator_sp(4) = ((atan2f(mix_solution(1), mix_solution(0)) / (tilt_servo_max))); //.7826f;
-        _actuator_sp(5) = 1.f - ((atan2f(mix_solution(3), mix_solution(2)) / (tilt_servo_max))); //.2174f;
-        _actuator_sp(6) = mix_solution(5); //mix_solution(5) / (elevon_max) + .5f; //0.f;
-        _actuator_sp(7) = mix_solution(6); //mix_solution(6) / (elevon_max) + .5f; //0.f;
+        // Actuator setpoints are solved based on the mix solution
+        // Throttle and servo angles are calculated from x and z components
+        _actuator_sp(0) = sqrt(mix_solution(0) * mix_solution(0) + mix_solution(1) * mix_solution(1));
+        _actuator_sp(1) = sqrt(mix_solution(2) * mix_solution(2) + mix_solution(3) * mix_solution(3));
+        _actuator_sp(2) = mix_solution(4);
+        _actuator_sp(3) = 0.f;
+        _actuator_sp(4) = atan2f(mix_solution(1), mix_solution(0)) / (tilt_servo_max);
+        _actuator_sp(5) = 1.f - atan2f(mix_solution(3), mix_solution(2)) / (tilt_servo_max);
+        _actuator_sp(6) = -mix_solution(5);
+        _actuator_sp(7) = mix_solution(6);
 
-
-        // PX4_INFO("actuator_setpoint: %.4f %.4f %.4f %.4f %.4f %.4f %.4f %.4f", (double) _actuator_sp(0), (double) _actuator_sp(1), (double) _actuator_sp(2),
-		//     (double) _actuator_sp(3), (double) _actuator_sp(4), (double) _actuator_sp(5), (double) _actuator_sp(6), (double) _actuator_sp(7));
 
         // Clip
         clipActuatorSetpoint(_actuator_sp);
 
+        // clipped_mix_solution performs backwards calculations to find mix_solution as adjusted for
+        // clipped actuator values
         matrix::Vector<float, NUM_ACTUATORS> clipped_mix_solution;
-        clipped_mix_solution(0) = _actuator_sp(0) / (float) sqrt(1 + pow(2, tanf(tilt_servo_max * _actuator_sp(4))));
-        clipped_mix_solution(1) = _actuator_sp(0) / (float) sqrt(1 + 1 / pow(2, tanf(tilt_servo_max * _actuator_sp(4))));
-        clipped_mix_solution(2) = _actuator_sp(1) / (float) sqrt(1 + pow(2, tanf(tilt_servo_max * (1.f - _actuator_sp(5)))));
-        clipped_mix_solution(3) = _actuator_sp(1) / (float) sqrt(1 + 1 / pow(2, tanf(tilt_servo_max * (1.f - _actuator_sp(5)))));
+        clipped_mix_solution(0) = _actuator_sp(0) /
+            (float) sqrt(1 + pow(tanf(tilt_servo_max * _actuator_sp(4)), 2));
+        clipped_mix_solution(1) = _actuator_sp(0) /
+            (float) sqrt(1 + 1 / pow(tanf(tilt_servo_max * _actuator_sp(4)), 2));
+        clipped_mix_solution(2) = _actuator_sp(1) /
+            (float) sqrt(1 + pow(tanf(tilt_servo_max * (1.f - _actuator_sp(5))), 2));
+        clipped_mix_solution(3) = _actuator_sp(1) /
+            (float) sqrt(1 + 1 / pow(tanf(tilt_servo_max * (1.f - _actuator_sp(5))), 2));
         clipped_mix_solution(4) = _actuator_sp(2);
-        clipped_mix_solution(5) = _actuator_sp(6); //(_actuator_sp(6) - .5f) * elevon_max;
-        clipped_mix_solution(6) = _actuator_sp(6); //(_actuator_sp(7) - .5f) * elevon_max;
+        clipped_mix_solution(5) = -_actuator_sp(6);
+        clipped_mix_solution(6) = _actuator_sp(7);
+
+        // Sign information is lost from squaring/taking the square root so we infer the clipped_mix_solution
+        // has the same sign as the original mix_solution
+        if ((mix_solution(2) < 0 && clipped_mix_solution(2) > 0) ||
+            (mix_solution(2) > 0 && clipped_mix_solution(2) < 0))
+            clipped_mix_solution(2) *= -1;
 
         // Compute achieved control
         _control_allocated = _effectiveness * clipped_mix_solution;
@@ -130,7 +107,6 @@ public:
 protected:
 
     DEFINE_PARAMETERS(
-		(ParamFloat<px4::params::CA_ELEVON_MAX>) _param_ca_elevon_max,
 		(ParamFloat<px4::params::CA_TLT_SRVO_MAX>) _param_ca_tlt_srvo_max
 	)
 };
